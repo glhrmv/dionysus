5311c2c6b30898594e0283302fe4967f451df708
Guilherme Vale <oguilhermevale@gmail.com>
2020-04-10 19:33:47 +0100

    Update styles and error page

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res) => {
   res.render('index', { title: 'Dionysus' });
+});
+
+router.post('/', (req, res) => {
+  res.send(req.body);
 });
 
 module.exports = router;
