0d0ea36974718fa5fb51be9e63460d967969d851
JoaoAlvaroFerreira <joaoalvaro.ferreira@hotmail.com>
2020-05-04 05:48:40 +0100

    added effects

--- public/scripts/elements.js
+++ public/scripts/elements.js
@@ -13,10 +13,26 @@ export const frequencyText = document.getElementById("frequency-text");
 export const qFactorControl = document.getElementById("q-factor");
 export const qFactorText = document.getElementById("q-factor-text");
 
+//effects
+//delay
+export const delayTimeControl = document.getElementById("delayTime");
+export const delayTimeText = document.getElementById("delayTime-text");
+export const feedbackControl = document.getElementById("feedback");
+export const feedbackText = document.getElementById("feedback-text");
+//reverb
+export const preDelayControl = document.getElementById("pre-delay");
+export const preDelayText = document.getElementById("pre-delay-text");
+export const decayControl = document.getElementById("decay");
+export const decayText = document.getElementById("decay-text");
+//bitcrush
+export const bitcrushControl = document.getElementById("bitcrush");
+export const bitcrushText = document.getElementById("bitcrush-text");
+
 export const playBtn = document.getElementById("play-btn");
 export const progressBar = document.getElementById("seek");
 export const startTime = document.getElementById("start-time");
 export const endTime = document.getElementById("end-time");
+
 
 export const waveCanvas = document.getElementById("waveform");
 export const freqCanvas = document.getElementById("freqbar");
